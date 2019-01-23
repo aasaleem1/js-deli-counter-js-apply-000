8a5e93f79dc0e4c01acf2bf723e579944958eecb
AJ Saleem <asaleem86@gmail.com>
2019-01-23 20:38:05 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+var katzDeliLine = [];
+
+takeANumber(katzDeliLine, "Ada");
+takeANumber(katzDeliLine), "Grace");
+takeANumber(katzDeliLine), "Kent");
+
+currentLine(katzDeliLine);
+
+nowServing(katzDeliLine);
+
+currentLine(katzDeliLine);
+
+takeANumber(katzDeliLine, "Matz");
+
+currentLine(katzDeliLine);
+
+nowServing(katzDeliLine); //currently serving Grace
+
+currentLine(katzDeliLine)
